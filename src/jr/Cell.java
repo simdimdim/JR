f9e24eb7fe2b016f5ca267733e84c7f5f2559d43
simdimdim <simdimdim@gmail.com>
2014-04-20 19:31:18 +0300

    latest

--- src/jr/Cell.java
+++ src/jr/Cell.java
@@ -15,9 +15,9 @@ import static jr.Main.*;
 public class Cell {
     boolean state=false;
     short cons=1;
-    int ID;
-    short board;
-    public Cell(int ID, short board){
+    final int ID;
+    final int board;
+    public Cell(int ID, int board){
         this.ID=ID;
         this.board=board;
     }
@@ -26,21 +26,40 @@ public class Cell {
         for(int neigh = -1; neigh <= 1; neigh++){
             for (int fut= -height; fut<=height;fut+=height){
                 var=fut+neigh+ID;
+                if (var==ID) continue;   // leave out self
                 if (tobe.containsKey(var)){
-                    if (var!=ID){
                     tobe.get(var).iter();
-                    }
                 }
                 else{
-                    tobe.put(var, new Cell(var, board));
+                    tobe.put(var, new Cell(var));
                 }
             }
         }
+        
     }
     void check(){
-        if (cons==2||cons==3){state=true;}
-        else{die();}
+        if (cons==2||cons==3)state=true;
+        else die();
     }
     public void iter(){cons++;}
     public void die(){tobe.remove(ID);}
+    
+    @Override 
+    public int hashCode() {
+        return ID;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cell other = (Cell) obj;
+        return this.ID !=other.ID;
+    }
+    public int getStateAsInt() {
+    return state==true ? 1 : 0;
+    }
 }
--- src/jr/Main.java
+++ src/jr/Main.java
@@ -1,21 +1,54 @@
 package jr;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 class Main {
     boolean running=true;
     int height = 10;
-    Map<Integer,Cell> process = new HashMap();
-    static Map<Integer,Cell> tobe = new HashMap();
-    public void run(){
-    while (running){
-        if (process.isEmpty()){
-            process=tobe;
-        }
-        else{
-            
+    int board=0;
+    static Map<Integer,Board> all = new HashMap();
+    
+    private class Board{
+       final Map<Integer,Cell> process = new HashMap();
+       final Map<Integer,Cell> tobe = new HashMap(); 
+       /** 
+     * Prints the state of the board into human sensible String output.  
+     * <p> 
+     * Use for debug. 
+     * @return string representation of the board 
+     */ 
+        @Override 
+        public String toString() { 
+            String out; 
+            for(int y=1;y<=height;y++) { 
+                for(x=1;x<=width; x++) { 
+                    out += process.get(Cell.generateHash(x,y)).stateAsInt()+" "; 
+                } 
+                out += "\n"; 
+            } 
+            return out; 
         }
     }
-    } 
+    
+    public void run(){
+        create();
+        while (running){
+            if (!process.isEmpty()){
+                Iterator<Map.Entry<Integer,Cell>> iter = process.entrySet().iterator();
+                while (iter.hasNext()&&running){
+                    Map.Entry<Integer,Cell> entry = iter.next();
+                    entry.getValue().spawn(height);
+                }
+            }
+            else{
+                process=tobe;
+            }
+        }
+    }
+    private void create(){
+        board=all.size(); //needs work (for deleting boards)
+        all.put(board, new Board());
+    }
 }
