01989bc087f0ea244f5c2c1465573a3c6038126e
simdimdim <simdimdim@gmail.com>
2014-04-23 17:03:37 +0300

    Release.

--- src/jr/Main.java
+++ src/jr/Main.java
@@ -12,32 +12,22 @@ class Main {
     static public void run(){
         for (int s=0; s<4; s++) {
             System.out.println("Step:"+s);
-            // use this instead of calling all.get[x] every time 
             BoardManager b = all.get(board);
 
             //main operations
-            
             //Gets list of all mates
             b.cur.board.values().stream().flatMap(cell-> //Gets list of all mates
                 cell.getNeighbours().stream()).distinct().forEach((Point p)->{
                     
-                    //forEach potential alive cell
-                    //checks alive neighbours and adds to next if alive
-                    if(b.cur.board.containsKey(p)){
-                        //System.out.println(p);
-                        if(b.check(p)==2){b.next.put(p);}
-                        //System.out.println(cur.getCell(p).mates);
-                    }
+                    //forEach mate if alive adds to next 
+                    if(b.check(p)==2&&b.cur.board.containsKey(p)){b.next.put(p);}
                     else{
                         if (b.check(p)==3) {
-                            //System.out.println(p);
                             b.next.put(p);}
                     }
                 });
             System.out.println("board alive \n"+b.ccheck(b.cur));
-            //System.out.println("board mates \n"+b.ccheck(b.cur));
-            //System.out.println("next alive \n"+b.acheck(b.next));
-            System.out.println("next mates \n"+b.ccheck(b.next));
+            //System.out.println("next mates \n"+b.ccheck(b.next));
             
             // stop simulation if no cells remain or eventual input
             if(b.cur.board.isEmpty()) {
@@ -55,9 +45,10 @@ class Main {
         all.add(new BoardManager(width,height));
         
         BoardManager b = all.get(board);
+        
         //Add some cells
-        //b.cur.put(new Cell(1,1).alive());
-        b.cur.put(new Cell(2,3));
+        b.cur.put(new Cell(1,1));
+        b.cur.put(new Cell(1,2));
         b.cur.put(new Cell(2,1));
         b.cur.put(new Cell(2,2));
     }
